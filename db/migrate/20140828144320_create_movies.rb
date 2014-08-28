a74d83140dbc2f8d9d809ca6b36ea3976a3e046b
Andrew Garcia <drewgarcia23@gmail.com>
2014-08-28 12:45:17 -0500

    First commit, Added Index View and Stylesheets

---
+++ db/migrate/20140828144320_create_movies.rb
@@ -0,0 +1,13 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+    	t.string :title
+    	t.string :format
+    	t.time :length
+    	t.integer :release
+    	t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
