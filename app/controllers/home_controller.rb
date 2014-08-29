c9d81c9601a41d749581cd403f5a4604e3203fb2
Andrew Garcia <drewgarcia23@gmail.com>
2014-08-28 22:13:20 -0500

    Added Remote Pry

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,6 +16,7 @@ class HomeController < ApplicationController
     end
   end
   def create
+    binding.remote_pry
   	Movie.addNewMovie(params[:title], params[:hours], params[:minutes], params[:rating], params[:release], params[:image_url], params[:format], params[:trailer])
     redirect_to :action=>"index", :controller=>"home"
   end
