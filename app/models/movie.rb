7f28d09022fa3645483b7703b6bba9a1d539ba25
Andrew Garcia <drewgarcia23@gmail.com>
2014-08-28 22:32:09 -0500

    Uncommented first line

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
 	def self.addNewMovie(title, hours, minutes, rating, release, image_url, format, trialer)
-		# theMovie = Movie.new(title: title, hours: hours, minutes: minutes, format: format, release: release, rating: rating, image_url: image_url, trialer: trialer)
+		theMovie = Movie.new(title: title, hours: hours, minutes: minutes, format: format, release: release, rating: rating, image_url: image_url, trialer: trialer)
 		# theMovie.save
 	end
 end
