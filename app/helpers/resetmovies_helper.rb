8405c3d51b8287953622a80d6ce83703b96cc865
Andrew Garcia <drewgarcia23@gmail.com>
2014-08-29 09:28:05 -0500

    Added reset movies

---
+++ app/helpers/resetmovies_helper.rb
@@ -0,0 +1,2 @@
+module ResetmoviesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'resetmovies/index'
+
   get 'start/index'
 
   get 'home/index'
   root 'start#index'
   resources :home
   resources :start
+  resources :resetmovies
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
